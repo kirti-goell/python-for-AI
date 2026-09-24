1a3f7419f3a5c46c7b04fc75e50739f4cf4d10d3
kirti-goell <kirtigoel2005@gmail.com>
2026-09-24 13:43:38 +0530

    cloning repo from github

---
+++ git-vs-github.py
@@ -0,0 +1,59 @@
+# GIT : git is like a save system for your code.It tracks every change you make , lets you go back to previous versions 
+# and lets you collaborate with other members of your team
+
+# GIT Hub : git hub is a place , where you store your git projects online - you can think of it as google drive for your code.
+
+# how git works
+# 1. you write code : make changes to your file
+# 2. you save a snapshot : called "commit" in git
+# 3. git remembers snapshot : who made it , when and why
+
+
+# Key concepts  
+# 1. repository : a project tracked by git
+# ex.:= git init krne pr bnta h git repo
+
+# 2. staging : choosing which changes to save
+# ex.:= selecting items before putting them in box for shipping
+
+# 3. commit : saved snapshot of your code
+# ex.:= like saving in a video game you can always go back
+
+# 4. push : upload your commits to git hub
+# backs up your code online
+
+# 5. pull : download chnages from git hub
+#  get updates when working with others
+
+# 6. clone : copy a project from git hub
+
+# to check if git is already in our comp or not
+# git --version
+
+
+# simplest git workflow
+# 1. first time only - initialize git in your project
+# git init
+# local git repo : python for AI(same name of project folder automatically chka isne jo mne project folder ko dia tha)
+
+# 2.lets stage or select files
+# git add .
+
+# 3. commit krdia
+# git commit -m "message goes here"
+
+# git status : it will show that whatever is left for commit , etc
+# That's it ! your code is saved and backed up
+
+
+# i just went to github and created github repo with name python-for-AI
+
+# clone : download existing project from github
+# create : starts new project and upload them
+
+# lets learn how to clone a repo
+# 1. i have searched a repo on github
+# 2. copied the url from github me "code" krke aega likha hua
+# 3. created folder on desktop named cloning and open the terminal inside this folder
+# 4. run command - : git clone url press enter
+
